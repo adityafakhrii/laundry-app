beb377cc8cddc16a3bf582004638c61978532199
Aditya Fakhri <adityafakhri03@gmail.com>
2020-04-22 21:22:02 +0700

    ubah title

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,8 +4,9 @@
     <title>Login | Dirtless</title>
     <link rel="stylesheet" type="text/css" href="asset/login/css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    
     <script src="https://kit.fontawesome.com/095e56106e.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="{{asset('asset/fontawesome/css/all.css')}}"></link>
+    <link rel="stylesheet" type="text/css" href="asset/fontawesome/css/all.css"></link>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="asset/img/icon.svg">
 </head>
